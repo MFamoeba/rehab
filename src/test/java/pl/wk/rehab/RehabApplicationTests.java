a090c01b27bb5c3f0ad6da3b7965db532031054c
mfamoeba <wkulesza298@gmail.com>
2024-12-20 16:08:13 +0100

    init

---
+++ src/test/java/pl/wk/rehab/RehabApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wk.rehab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RehabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
